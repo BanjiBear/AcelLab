dec351bad68d20c3c4e89e09a2186e0a9c9f50c1
samCks7600 <a63492690@gmail.com>
2024-08-05 13:55:22 +0800

    [File]Project Start

---
+++ src/test/java/io/acellab/service/web/app/startline/official/acelLab/AcelLabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acellab.service.web.app.startline.official.acelLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcelLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
