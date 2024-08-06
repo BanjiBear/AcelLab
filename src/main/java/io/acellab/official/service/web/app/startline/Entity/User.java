bd16ea1903f8556f1b6da45d8f588b0e6a558c32
samCks7600 <a63492690@gmail.com>
2024-08-06 15:05:16 +0800

    [Code]Update User Entity

---
+++ src/main/java/io/acellab/official/service/web/app/startline/Entity/User.java
@@ -0,0 +1,37 @@
+package io.acellab.official.service.web.app.startline.Entity;
+
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "crm_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long userId;
+
+    @Column(name = "username", nullable = true)
+    private String username;
+
+    @Column(name = "passwd", nullable = true)
+    private String password;
+
+    @Column(name = "salt", nullable = true)
+    private String passwordSalt;
+
+    @Column(name = "nickname", nullable = true)
+    private String nickname;
+
+
+}
