6de7ed8b287450170ace8a0dbd0f2c5712d67c2d
samCks7600 <a63492690@gmail.com>
2024-08-08 16:03:46 +0800

    [Code]Update CustomUserDetails #Delete full name parameter

--- src/main/java/io/acellab/official/service/web/app/startline/Config/Security/CustomUserDetails.java
+++ src/main/java/io/acellab/official/service/web/app/startline/Config/Security/CustomUserDetails.java
@@ -10,18 +10,12 @@ public class CustomUserDetails implements UserDetails {
     private String username;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
-    private String fullname;
 
     public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities,
                              String fullname) {
         this.username = username;
         this.password = password;
         this.authorities = authorities;
-        this.fullname = fullname;
-    }
-
-    public String getFullname() {
-        return fullname;
     }
 
     @Override
