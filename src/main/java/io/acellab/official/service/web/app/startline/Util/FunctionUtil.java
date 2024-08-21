1fa9cf2ef9ae56affabcd7782d0ddab841aca2ca
samCks7600 <a63492690@gmail.com>
2024-08-21 14:17:40 +0800

    [Code]Change password rule # change required password length to 12

--- src/main/java/io/acellab/official/service/web/app/startline/Util/FunctionUtil.java
+++ src/main/java/io/acellab/official/service/web/app/startline/Util/FunctionUtil.java
@@ -3,7 +3,7 @@ package io.acellab.official.service.web.app.startline.Util;
 public class FunctionUtil {
 
     public static boolean isValidPassword(String password) {
-        String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$";
         return password.matches(passwordRegex);
     }
 
