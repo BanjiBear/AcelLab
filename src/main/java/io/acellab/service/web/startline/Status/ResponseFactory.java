13eb7c577e41fa4c6a4a8efeb85a0d64c6e1e5ae
BanjiBear <tommychen900217@gmail.com>
2024-12-19 21:51:22 +0800

    [Fix] Fix getter/setter, typo on cases, Spring failed to retrieve functions automatically

--- src/main/java/io/acellab/service/web/startline/Status/ResponseFactory.java
+++ src/main/java/io/acellab/service/web/startline/Status/ResponseFactory.java
@@ -13,7 +13,7 @@ public class ResponseFactory<T> {
 	public int getStatusCode() {return this.status.getStatusCode();}
 	public String getStatusMessage() {return this.status.getStatusMessage();}
 
-	public void setreturnDataList(List<T> returnDataList) {this.returnDataList = returnDataList;}
-	public List<T> getreturnDataList() {return this.returnDataList;}
+	public void setReturnDataList(List<T> returnDataList) {this.returnDataList = returnDataList;}
+	public List<T> getReturnDataList() {return this.returnDataList;}
 	
 }
