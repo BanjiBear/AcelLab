b525dde2a31843d055ce6750ccb9147a6e360c22
BanjiBear <tommychen900217@gmail.com>
2025-02-02 23:23:35 +0800

    [Code][Frontend][Fix] Add terms and privacy, add missing image in support page

--- src/main/resources/static/js/payment.js
+++ src/main/resources/static/js/payment.js
@@ -1,6 +1,6 @@
 //Only allow selection of 1 plan
 function updateCheck(checkboxId) {
-	var checkboxes = ["startup", "free", "business", "corporate"];
+	var checkboxes = ["startup", "free", "business", "enterprise"];
 
 	checkboxes.forEach(function (item) {
 		if (item !== checkboxId) {
@@ -9,15 +9,32 @@ function updateCheck(checkboxId) {
 	});
 }
 
-function pay() {
-	if (
-		document.getElementById("startup").checked ||
-		document.getElementById("free").checked ||
-		document.getElementById("business").checked ||
-		document.getElementById("corporate").checked
-	) {
-		document.getElementById("pay").style.display = "block";
-	} else {
+const form = document.getElementById('priceform');
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	let plans = ["startup", "free", "business", "enterprise"];
+	let hasplan = false;
+	let plan = undefined;
+	plans.forEach(function (item) {
+		element = document.getElementById(item);
+		if(element){
+			if(element.checked){
+				hasplan = true;
+				plan = element.value;
+			}
+		}
+	});
+
+	if(!hasplan){
 		alert("Please select a membership option.");
+	} else{
+		let hiddenInput = document.createElement('input');
+		hiddenInput.type = 'hidden';
+		hiddenInput.name = 'plan';
+		hiddenInput.value = plan;
+
+		this.appendChild(hiddenInput);
+		this.submit();
 	}
-}
+});
